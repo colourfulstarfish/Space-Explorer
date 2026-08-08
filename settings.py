753bfde3cc83533f8118e3a26742a2d613aa33a5
mattheson <yanjunjj7@gmail.com>
2026-08-08 16:59:13 +0800

    added enemy settings

--- settings.py
+++ settings.py
@@ -11,6 +11,8 @@ dark_blue=(0,0,153)
 light_blue=(2,136,209)
 orange=(246,178,107)
 yellow=(255,250,102)
+red=(220,50,50)
+b4o3n=(94,44,4)
 #player settings
 player_speed=1
 player_width=40
@@ -19,7 +21,10 @@ player_height=50
 bullet_speed=10
 bullet_width=6
 bullet_height=14
-
-
-
-
+#enemy settings
+enemy_speed=2
+enemy_fall_speed=1
+enemy_width=36
+enemy_height=30
+enemy_spawn_frames=90
+enemy_score=10
